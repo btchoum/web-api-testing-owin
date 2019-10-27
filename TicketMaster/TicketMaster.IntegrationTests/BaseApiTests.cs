5ffd76cf7da71c90844400c20da733416b6d46e0
btchoum <bertrand.tchoumkeu@gmail.com>
2019-10-27 13:27:24 -0500

    move OneTimeSetup to base class

---
+++ TicketMaster/TicketMaster.IntegrationTests/BaseApiTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+
+namespace TicketMaster.IntegrationTests
+{
+    public class BaseApiTests
+    {
+        protected HttpClient HttpClient;
+        protected IDisposable WebApp;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var _baseAddress = "http://localhost:9000/";
+
+            Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: _baseAddress);
+
+            var handler = new HttpClientHandler { UseDefaultCredentials = true };
+            HttpClient = new HttpClient(handler) { BaseAddress = new Uri(_baseAddress) };
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            if (HttpClient != null)
+            {
+                HttpClient.Dispose();
+                HttpClient = null;
+            }
+
+            if (WebApp != null)
+            {
+                WebApp.Dispose();
+                WebApp = null;
+            }
+        }
+    }
+}
--- TicketMaster/TicketMaster.IntegrationTests/TickerMasterApiShould.cs
+++ TicketMaster/TicketMaster.IntegrationTests/TickerMasterApiShould.cs
@@ -1,58 +1,18 @@
-﻿using Microsoft.Owin.Hosting;
-using NUnit.Framework;
-using System;
+﻿using NUnit.Framework;
 using System.Net.Http;
 
 namespace TicketMaster.IntegrationTests
 {
     [TestFixture]
-    public class TickerMasterApiShould
+    public class TickerMasterApiShould : BaseApiTests
     {
-        private HttpClient _httpClient;
-        private IDisposable _webApp;
-
-        [OneTimeSetUp]
-        public void OneTimeSetup()
-        {
-            var _baseAddress = "http://localhost:9000/";
-            
-            WebApp.Start<Startup>(url: _baseAddress);
-
-            var handler = new HttpClientHandler { UseDefaultCredentials = true };
-            _httpClient = new HttpClient(handler) { BaseAddress = new Uri(_baseAddress) };
-        }
-
-        [OneTimeTearDown]
-        public void OneTimeTearDown()
-        {
-            if (_httpClient != null)
-            {
-                _httpClient.Dispose();
-                _httpClient = null;
-            }
-
-            if (_webApp != null)
-            {
-                _webApp.Dispose();
-                _webApp = null;
-            }
-        }
-
-
         [Test]
         public void Get_should_be_successfull()
         {
-            var response = _httpClient.GetAsync("api/todos").Result;
+            var response = HttpClient.GetAsync("api/todos").Result;
 
             Assert.IsTrue(response.IsSuccessStatusCode, "Actual Status Code " + response.StatusCode);
         }
 
-        [Test]
-        public void Get_should_be_successfull_owin_selfhost()
-        {
-            var response = _httpClient.GetAsync("api/todos").Result;
-
-            Assert.IsTrue(response.IsSuccessStatusCode, "Actual Status Code " + response.StatusCode);
-        }
     }
 }
